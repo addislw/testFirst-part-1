4024fb8032ffb2a938ee4f2a19153f88206a95a9
addislw <addislw@gmail.com>
2020-08-23 18:07:57 -0400

    exercise 3 complete

--- 03-factory-functions/calculator.js
+++ 03-factory-functions/calculator.js
@@ -1,1 +1,31 @@
 /* eslint-disable no-unused-vars */
+/*
+function createCalculator() {
+  return {
+    total: 0,
+    add(x) {
+      this.total += x;
+    },
+  };
+}
+*/
+
+const createCalculator = () => {
+  let calculator = {
+    total: 0,
+    add(x) {
+      this.total += x;
+    },
+    subtract(x) {
+      this.total -= x;
+    },
+    value() {
+      return this.total;
+    },
+    clear() {
+      this.total = 0;
+    },
+  };
+
+  return calculator;
+};
--- 04-loops/loops.js
+++ 04-loops/loops.js
@@ -1,1 +1,50 @@
 /* eslint-disable no-unused-vars */
+const repeat = (str, num) => {
+  newStr = '';
+  while (num > 0) {
+    newStr += str;
+    num--;
+  }
+  return newStr;
+};
+
+function sum(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else if (arr.length > 0) {
+    let tot = 0;
+    for (let i = 0; i < arr.length; i++) {
+      tot += arr[i];
+    }
+    return tot;
+  }
+}
+
+const join = (arr, delim) => {
+  let str = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    str += arr[i];
+    if (delim !== undefined && i < arr.length - 1) {
+      str += delim;
+    }
+  }
+  return str;
+};
+
+function gridGenerator(n) {
+  retStr = '';
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if ((i + j) % 2 === 0) {
+        retStr += '#';
+      } else {
+        retStr += ' ';
+      }
+    }
+    retStr += '\n';
+  }
+  return retStr;
+}
+console.log(gridGenerator(2));
